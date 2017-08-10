9eb9b68547a8263d0af5af98e3c687a4b14a715c
'renjianguo' <'1358942343@qq.com'>
2017-08-10 21:46:39 +0800

    又上传了一个个

---
+++ java/src/main/java/com/xhhy/java/MyApp.java
@@ -0,0 +1,8 @@
+package com.xhhy.java;
+
+public class MyApp {
+	
+	public void sayHello(){
+	System.out.println("hello world");	
+	}
+	}
---
+++ java/src/test/java/com/xhhy/java/MyAppTest.java
@@ -0,0 +1,16 @@
+package com.xhhy.java;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class MyAppTest extends TestCase{
+	
+	    public void testMyApp()
+	    {
+	    	MyApp mt = new MyApp();
+	    	mt.sayHello();
+	        /*assertTrue( true );*/
+	    }
+
+}
